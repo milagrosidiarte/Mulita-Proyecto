f1fdad424642d954dfaea4f47711ccfb8a4b6a24
milagrosidiarte <milagros.idiarte@uap.edu.ar>
2026-07-10 17:53:22 -0300

    Comentarios de supabase.ts

--- mulita-web/src/lib/supabase.ts
+++ mulita-web/src/lib/supabase.ts
@@ -6,9 +6,9 @@ export const createClientSupabase = () => {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      auth: {
-        persistSession: true,
-        storage: typeof window !== 'undefined' ? localStorage : undefined,
+      auth: { // Configuración de autenticación
+        persistSession: true, // Permite que la sesión se mantenga entre recargas de página
+        storage: typeof window !== 'undefined' ? localStorage : undefined, // Usa localStorage para persistir la sesión en el navegador, pero no en el servidor
       },
     }
   );
